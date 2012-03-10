4d67e106f85eb8548ef529ddf2964903eac47495
Raymond Ellis <raymondellis@live.com>
2012-03-10 15:59:46 -0800

    Java: toString may be done. Still needs testing.

--- Java/SimpleD/src/simpleD/Group.java
+++ Java/SimpleD/src/simpleD/Group.java
@@ -1,19 +1,21 @@
 package simpleD;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class Group {
-	public String Name;
+	public String name;
 	
-	public ArrayList<Group> Groups;
-	public ArrayList<Property> Properties;
+	public ArrayList<Group> Groups = new ArrayList<Group>();
+	public List<Property> Properties = new ArrayList<Property>();
 	
 	public Style BraceStyle = Style.BSD_Allman;
 	public String Tab="\t";
 	
 	public Group(){}
-	public Group(String name){
-		Name=name;
+	public Group(String Name){
+		name=Name;
 	}
 	
 	public enum Style{
@@ -25,13 +27,90 @@ public class Group {
 		GroupsOnNewLine
 	}
 	
-	public String ToString(Boolean AddVersion){
-		return ToString(true, -1, AddVersion);
+	public String toString(Boolean AddVersion){
+		return toString(true, -1, AddVersion, BraceStyle);
 	}
-	String ToString(Boolean isFirst, int tabCount, Boolean AddVersion){
+	String toString(Boolean isFirst, int tabCount, Boolean AddVersion, Style braceStyle){
+		if(Properties.isEmpty() & Groups.isEmpty())
+			return "";
 		
+		if(tabCount <-1) tabCount=-2;
 		
-		return "";
+		//Brace styling
+		if (this.BraceStyle==Style.None) braceStyle=this.BraceStyle;
+		if(braceStyle==Style.None) braceStyle=Style.BSD_Allman;
+		
+		String tmp="";
+		
+		if(AddVersion) tmp="SimpleD{Version="+Info.Version+";FormatVersion="+Info.FileVersion+";}";
+		
+		//Name and start of group.  Name{
+		if(!isFirst){
+			switch (braceStyle){
+				case NoStyle: case K_R:
+					tmp += name + "{";
+					break;
+				case Whitesmiths:
+					tmp += name + "\n" + getTabs(tabCount +1) +"{";
+					break;
+				case BSD_Allman:
+					tmp += name+"\n"+getTabs(tabCount)+"{";
+					break;
+				case GNU:
+					tmp += name+"\n"+getTabs(tabCount)+"  {";
+					break;
+				case GroupsOnNewLine:
+					tmp += "\n"+getTabs(tabCount-1)+name+"{";
+					break;
+			}
+		}
+		
+		//Groups and properties.
+		switch(braceStyle){
+			case NoStyle: case GroupsOnNewLine:
+				for(int i=0; i<Properties.size(); i++){
+					tmp += Properties.get(i).name+"="+Properties.get(i).value+";";
+				}
+				for(int i=0; i<Groups.size(); i++){
+					tmp+=Groups.get(i).toString(false, tabCount+1, false, braceStyle);
+				}
+				break;
+			case Whitesmiths: case BSD_Allman: case K_R: case GNU:
+				for(int i=0; i<Properties.size(); i++){
+					tmp +="\n"+getTabs(tabCount+1)+ Properties.get(i).name+"="+Properties.get(i).value+";";
+				}
+				for(int i=0; i<Groups.size(); i++){
+					tmp+="\n"+getTabs(tabCount+1)+ Groups.get(i).toString(false, tabCount+1, false, braceStyle);
+				}
+				break;
+		}
+		
+		//} end of group.
+		if(!isFirst){
+			switch(braceStyle){
+				case NoStyle: case GroupsOnNewLine:
+					tmp+="}";
+					break;
+				case Whitesmiths:
+					tmp+="\n"+getTabs(tabCount+1)+"}";
+					break;
+				case BSD_Allman: case K_R:
+					tmp+="\n"+getTabs(tabCount)+"}";
+					break;
+				case GNU:
+					tmp+="\n"+getTabs(tabCount)+"  }";
+					break;
+			}
+		}
+		
+		return tmp;
 	}
 	
+	private String getTabs(int count){
+		StringBuilder sb = new StringBuilder();
+		for (int i =0; i<count; i++){
+			sb.append(Tab);
+		}
+		return sb.toString();
+	}
 }
--- Java/SimpleD/src/simpleD/Property.java
+++ Java/SimpleD/src/simpleD/Property.java
@@ -1,5 +1,12 @@
 package simpleD;
 
 public class Property {
-
+	
+	public String name;
+	public String value;
+	
+	public Property(String Name, String Value){
+		name=Name;
+		value=Value;
+	}
 }
